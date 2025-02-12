af2d2727aa6900103f12bc3b6194a34e08cbfac9
Prathamesh Patil <94956959+Prath7057@users.noreply.github.com>
2025-02-12 22:25:02 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" />
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
-<link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
+    <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
 
     <style>
         option {
@@ -19,6 +19,15 @@
 
         select {
             outline: 1px solid black;
+        }
+
+        select {
+            -webkit-appearance: none;
+            scrollbar-width: none;
+        }
+
+        select::-webkit-scrollbar {
+            display: none;
         }
     </style>
 </head>
